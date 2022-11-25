19749803d821e82a0c908a87751f55685f8a16b5
MerlyCampo <merlycampogue@hotmail.com>
2022-11-25 10:31:45 -0500

    Update App.jsx
    actualización de rutas App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,12 @@ import LayoutAuth from "./Layout/LayoutAuth";
 import RutaProtegida from "./Layout/RutaProtegida";
 // pages (usuarios)
 import Login from "./Pages/Login";
-import Registro from "./Pages/usuario/Registro";
-import OlvidePassword from "./Pages/usuario/OlvidePassword";
-import Confirmar from "./Pages/usuario/Confirmar";
+import Registro from "./Pages/Usuario/Registro";
+import OlvidePassword from "./Pages/Usuario/OlvidePassword";
+import Confirmar from "./Pages/Usuario/Confirmar";
 // pages (usuarios protegidas)
-import Perfil from "./Pages/usuario/Perfil";
-import CambiarPassword from "./Pages/usuario/CambiarPassword.jsx";
+import Perfil from "./Pages/Usuario/Perfil";
+import CambiarPassword from "./Pages/Usuario/CambiarPassword.jsx";
 // pages (productos)
 // import ListaProductos from "./pages/productos/ListaProductos";
 // import FormularioProductos from "./pages/productos/FormularioProductos";
